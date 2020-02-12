5bb45d350e28a3ebbebb71d00baf6f27fb0e7caf
candrowm <candrowm@hu-berlin.de>
2020-02-12 19:56:14 +0100

    added suggesty to filter pipeline

--- controller/oauthController.js
+++ controller/oauthController.js
@@ -4,10 +4,6 @@ var NodeGeocoder = require('node-geocoder');
 const geolib = require('geolib');
 
 
-
-
-
-
 function getLongLat(userZipCity) {
 
   return new Promise(resolve => {
@@ -20,18 +16,17 @@ function getLongLat(userZipCity) {
     };
 
     var geocoder = NodeGeocoder(options);
-    geocoder.geocode(userZipCity).then(function(res) {
-        var longLat = {"longitude" : res[0].longitude, "latitude" : res[0].latitude};
-          resolve(longLat);
-      })
+    geocoder.geocode(userZipCity).then(function (res) {
+      var longLat = {"longitude": res[0].longitude, "latitude": res[0].latitude};
+      resolve(longLat);
+    })
   });
 }
 
 
-
 exports.notify = async (req, res) => {
   try {
-    if(!req.headers.authorization){
+    if (!req.headers.authorization) {
       res.status(400).json({
         success: false,
         error: "unauthorized, access token required"
@@ -40,18 +35,22 @@ exports.notify = async (req, res) => {
     } else {
 
 
-      var access_token = req.headers.authorization.replace("Bearer ","");
+      var access_token = req.headers.authorization.replace("Bearer ", "");
       const p = await fetchProfile(access_token)
-     var userZip = p.profiles[0].supporter.address[0].zip;
-     var userCity = p.profiles[0].supporter.address[0].city;
-     var userId = p.id;
+      var userZip = p.profiles[0].supporter.address[0].zip;
+      var userCity = p.profiles[0].supporter.address[0].city;
+      var userId = p.id;
+
 
       const s = await fetchEvents("code");
 
+      const recommendedEvents = await fetchRecommendedEvents("code");
+
       var poolEvents = [];
 
-      for(var j = 0; j < s.data.length; j++){
+      for (var j = 0; j < s.data.length; j++) {
         var poolEvent = s.data[j];
+        var poolEventId = poolEvent.id;
         var poolEventLat = poolEvent.latitude;
         var poolEventLong = poolEvent.longitude;
         var poolEventName = poolEvent.name;
@@ -64,24 +63,57 @@ exports.notify = async (req, res) => {
 
         var poolEventGeo = {latitude: poolEvent.latitude, longitude: poolEvent.longitude};
         var userGeo = {latitude: result.latitude, longitude: result.longitude};
-        var resss = geolib.getDistance(poolEventGeo, userGeo);
-        var distanceInKm = resss / 1000;
+        var distanceUserPoolEventInMeters = geolib.getDistance(poolEventGeo, userGeo);
+        var poolEventDistanceInKilometers = distanceUserPoolEventInMeters / 1000;
 
-        var notifyParameters = {"poolEventName": poolEventName,
-                                "poolEventLat": poolEventLat,
-                                "poolEventLong": poolEventLong,
-                                "poolEventCity": poolEventCity,
-                                "poolEventDate": poolEventDate};
+        // Favorite Artist?
+        var newNotifyAdded = false;
+        for (var x = 0; x < recommendedEvents.data.length; x++) {
+          if (poolEventId == recommendedEvents.data[x].actionId) {
+            var poolEventFavoriteArtistName = recommendedEvents.data[x].artistName;
 
+            var notifyParameters = {
+              "poolEventName": poolEventName,
+              "poolEventLat": poolEventLat,
+              "poolEventLong": poolEventLong,
+              "poolEventCity": poolEventCity,
+              "poolEventDate": poolEventDate,
+              "poolEventDistanceKm": poolEventDistanceInKilometers,
+              "poolEventFavoriteArtist": poolEventFavoriteArtistName
+            };
 
-        var notify = {"notifyMicroservice": "WAVES",
-          "notifyCreatedAt": poolEventCreatedAt,
-          "notifyLink": "#",
-          "notifyParameters": notifyParameters
+            var notify = {
+              "notifyMicroservice": "SUGGESTY",
+              "notifyCreatedAt": poolEventCreatedAt,
+              "notifyLink": "#",
+              "notifyParameters": notifyParameters
+            }
+            poolEvents.push(notify);
+            newNotifyAdded = true;
+            break;
+          }
         }
 
-        poolEvents.push(notify);
 
+        // Distance < 1000 km?
+        if (!newNotifyAdded && poolEventDistanceInKilometers < 1000) {
+          var notifyParameters = {
+            "poolEventName": poolEventName,
+            "poolEventLat": poolEventLat,
+            "poolEventLong": poolEventLong,
+            "poolEventCity": poolEventCity,
+            "poolEventDate": poolEventDate,
+            "poolEventDistanceKm": poolEventDistanceInKilometers
+          };
+
+          var notify = {
+            "notifyMicroservice": "WAVES",
+            "notifyCreatedAt": poolEventCreatedAt,
+            "notifyLink": "#",
+            "notifyParameters": notifyParameters
+          }
+          poolEvents.push(notify);
+        }
       }
     }
 
@@ -97,12 +129,9 @@ exports.notify = async (req, res) => {
 };
 
 
-
-
-
 exports.authenticate = async (req, res) => {
   try {
-    const { code, state } = req.query;
+    const {code, state} = req.query;
     const s = await fetchToken(code);
 
     const p = await fetchProfile(s.access_token)
@@ -128,7 +157,7 @@ exports.authenticate = async (req, res) => {
 
 const fetchToken = async code => {
   try {
-    const { data } = await Axios.get(
+    const {data} = await Axios.get(
       `https://stage.vivaconagua.org/drops/oauth2/access_token?grant_type=authorization_code&client_id=notify&code=${code}&redirect_uri=http://localhost:8005/v1/events/oauth`
     );
     return data;
@@ -139,7 +168,7 @@ const fetchToken = async code => {
 
 const fetchProfile = async access_token => {
   try {
-    const { data } = await Axios.get(
+    const {data} = await Axios.get(
       "https://stage.vivaconagua.org/drops/oauth2/rest/profile?access_token=" +
       access_token
     );
@@ -150,9 +179,24 @@ const fetchProfile = async access_token => {
   }
 };
 
+const fetchRecommendedEvents = async code => {
+
+  try {
+    const {data} = await Axios.get(
+      `http://localhost:5001/suggesty/api/v1/spotify/suggestion/user/a1f198b5-09f0-4271-b3b3-89e4a0e655e7`
+    );
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+
+
+}
+
 const fetchEvents = async code => {
   try {
-    const { data } = await Axios.get(
+    const {data} = await Axios.get(
       `http://localhost:5000/waves/api/v1/poolevent/notify`
     );
     return data;
@@ -160,7 +204,6 @@ const fetchEvents = async code => {
     throw error;
   }
 };
-
 
 
 exports.receiveToken = (req, res) => {
