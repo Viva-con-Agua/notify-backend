3d7cb9fa375ce3a9ee6aa5e7a7336fbaa2eb91a9
Nicola <nicola@schulze-sulingen.de>
2020-02-15 13:56:04 +0100

    build pipeline for necessity, added sql request for status, didnt merge with suggesty commit yet

--- controller/notifyController.js
+++ controller/notifyController.js
@@ -2,27 +2,46 @@
 // @route GET /api/v1/:id
 // @access Public
 exports.getNotificationsByVcaId = (req, res) => {
-
   var vcaId = req.params.vcaId;
 
-  if(true){
+  if (true) {
     res.status(200).json({
       success: true,
       data: vcaId
     });
   }
-
 };
+// exports.getStatus = async (type, userId, pooleventId) => {
+//   // let { pooleventId, userId, type } = req.query;
+//   // console.log(pooleventId);
+//   // console.log(userId);
+//   // console.log(type);
 
+//   const sql = `SELECT status
+//               FROM notifications
+//               WHERE type_id='${pooleventId}' AND user_id = '${userId}' AND type = '${type}';`;
+//   console.log(sql);
+//   global.conn.query(sql, (err, data) => {
+//     if (err) {
+//       throw error;
+//     } else {
+//       console.log(data);
 
+//       return data;
+//     }
+//   });
+// };
 
-
-
-
-
-
-
-
-
-
-
+exports.getStatus = (type, userId, pooleventId) =>
+  new Promise((resolve, reject) => {
+    const sql = `SELECT status
+                FROM notifications
+                WHERE type_id='${pooleventId}' AND user_id = '${userId}' AND type = '${type}';`;
+    global.conn.query(sql, (err, status) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(status);
+      }
+    });
+  });
--- controller/oauthController.js
+++ controller/oauthController.js
@@ -1,111 +1,172 @@
 const Axios = require("axios");
-
-var NodeGeocoder = require('node-geocoder');
-const geolib = require('geolib');
-
-
-
-
-
+const { getStatus } = require("../controller/notifyController");
+var NodeGeocoder = require("node-geocoder");
+const geolib = require("geolib");
+const var_dump = require("var_dump");
 
 function getLongLat(userZipCity) {
-
   return new Promise(resolve => {
-
     var options = {
-      provider: 'google',
-      httpAdapter: 'https', // Default
+      provider: "google",
+      httpAdapter: "https", // Default
       apiKey: process.env.GOOGLE_AUTH,
-      formatter: null         // 'gpx', 'string', ...
+      formatter: null // 'gpx', 'string', ...
     };
 
     var geocoder = NodeGeocoder(options);
     geocoder.geocode(userZipCity).then(function(res) {
-        var longLat = {"longitude" : res[0].longitude, "latitude" : res[0].latitude};
-          resolve(longLat);
-      })
+      var longLat = { longitude: res[0].longitude, latitude: res[0].latitude };
+      resolve(longLat);
+    });
   });
 }
 
-
-
 exports.notify = async (req, res) => {
+  console.log("Get new poolevents!");
   try {
-    if(!req.headers.authorization){
-      res.status(400).json({
-        success: false,
-        error: "unauthorized, access token required"
-      });
+    // if (!req.headers.authorization) {
+    //   res.status(400).json({
+    //     success: false,
+    //     error: "unauthorized, access token required"
+    //   });
+    // } else {
+    var access_token = req.headers.authorization.replace("Bearer ", "");
+    const p = await fetchProfile(access_token);
+    var userZip = p.profiles[0].supporter.address[0].zip;
+    var userCity = p.profiles[0].supporter.address[0].city;
+    var userId = p.id;
 
-    } else {
+    //First step: Get interesting new Events for user
+    //-----------------------------------------------
+    const s = await fetchEvents("code");
 
+    var poolEvents = [];
 
-      var access_token = req.headers.authorization.replace("Bearer ","");
-      const p = await fetchProfile(access_token)
-     var userZip = p.profiles[0].supporter.address[0].zip;
-     var userCity = p.profiles[0].supporter.address[0].city;
-     var userId = p.id;
+    const result = await getLongLat(userZip + " " + userCity);
+    var userGeo = {
+      latitude: result.latitude,
+      longitude: result.longitude
+    };
+    console.log("User: ", result);
+    counter = 0;
 
-      const s = await fetchEvents("code");
+    // var_dump(s.data);
 
-      var poolEvents = [];
+    var filteredEvents = [];
 
-      for(var j = 0; j < s.data.length; j++){
-        var poolEvent = s.data[j];
-        var poolEventLat = poolEvent.latitude;
-        var poolEventLong = poolEvent.longitude;
-        var poolEventName = poolEvent.name;
-        var poolEventCreatedAt = poolEvent.created_at;
-        var poolEventCity = poolEvent.locality;
-        var poolEventDate = poolEvent.event_start;
+    s.data.forEach(poolEvent => {
+      //Awareness Pipeline: Necessity
+      //Get all Events with number_of_applications < (supp_lim -3) AND application_end < 5 days in the future
+      //--------------------------------
+      var date = new Date();
+      var last = date.getTime() - 5 * 24 * 60 * 60 * 1000;
 
-        const result = await getLongLat(userZip + " " + userCity)
+      console.log(
+        "application_date soon: ",
+        Date.parse(poolEvent.application_end) > last
+      );
+      console.log(
+        "supporter neede: ",
+        poolEvent.applications < poolEvent.supporter_lim - 3
+      );
 
-
-        var poolEventGeo = {latitude: poolEvent.latitude, longitude: poolEvent.longitude};
-        var userGeo = {latitude: result.latitude, longitude: result.longitude};
-        var resss = geolib.getDistance(poolEventGeo, userGeo);
-        var distanceInKm = resss / 1000;
-
-        var notifyParameters = {"poolEventName": poolEventName,
-                                "poolEventLat": poolEventLat,
-                                "poolEventLong": poolEventLong,
-                                "poolEventCity": poolEventCity,
-                                "poolEventDate": poolEventDate};
-
-
-        var notify = {"notifyMicroservice": "WAVES",
-          "notifyCreatedAt": poolEventCreatedAt,
-          "notifyLink": "#",
-          "notifyParameters": notifyParameters
+      if (
+        poolEvent.applications < poolEvent.supporter_lim - 3 &&
+        Date.parse(poolEvent.application_end) > last
+      ) {
+        poolEvent.Microservice = "WAVES.Necessity";
+        filteredEvents.push(poolEvent);
+        console.log("NEC: ", poolEvent.name);
+      } else {
+        //Awareness Pipeline: Distance
+        //Get all Events in 500 km Radius
+        //--------------------------------
+        if (
+          typeof poolEvent.latitude === "undefined" ||
+          typeof poolEvent.longitude === "undefined"
+        ) {
+          return true;
         }
 
-        poolEvents.push(notify);
+        var poolEventGeo = {
+          latitude: poolEvent.latitude,
+          longitude: poolEvent.longitude
+        };
 
+        const distance = geolib.getDistance(poolEventGeo, userGeo);
+        const distanceInKm = distance / 1000;
+        if (distanceInKm <= 500) {
+          poolEvent.Microservice = "WAVES.Distance";
+          filteredEvents.push(poolEvent);
+          console.log("LOC: ", poolEvent.name);
+        }
       }
-    }
-
-    res.json(poolEvents);
-
-  } catch (error) {
-
-    res.status(400).json({
-      success: false,
-      error: error
+      //Awareness Pipeline: Recomendations
+      //Von Suggesty
+      //--------------------------------
     });
+    // var_dump(filteredEvents);
+
+    var poolEventStatus;
+    var promises = [];
+
+    filteredEvents.forEach(poolEvent => {
+      var promise = getStatus("event", userId, poolEvent.id).then(
+        poolEventStatusResult => {
+          poolEventStatus =
+            poolEventStatusResult.length > 0
+              ? poolEventStatusResult[0].status
+              : "new"; //resp from array to string
+
+          console.log("Status1", poolEventStatus);
+
+          if (poolEventStatus === "deleted") {
+            return;
+          }
+
+          var notifyParameters = {
+            poolEventName: poolEvent.name,
+            poolEventLat: poolEvent.latitude,
+            poolEventLong: poolEvent.longitude,
+            poolEventCity: poolEvent.locality,
+            poolEventDate: poolEvent.event_start
+          };
+
+          var notify = {
+            notifyMicroservice: poolEvent.Microservice,
+            notifyCreatedAt: poolEvent.created_at,
+            notifyLink: "#",
+            notifyStatus: poolEventStatus,
+            notifyParameters: notifyParameters,
+            notifyType: "ACTION"
+          };
+
+          // var_dump(notify);
+
+          poolEvents.push(notify);
+        }
+      );
+      promises.push(promise);
+    });
+
+    Promise.all(promises).then(() => {
+      // var_dump(poolEvents);
+      res.json(poolEvents);
+    });
+  } catch (error) {
+    console.log("Error in getNotifications: ", error);
   }
 };
-
-
-
-
 
 exports.authenticate = async (req, res) => {
   try {
     const { code, state } = req.query;
+    console.log(code);
     const s = await fetchToken(code);
+    console.log(s);
 
-    const p = await fetchProfile(s.access_token)
+    const p = await fetchProfile(s.access_token);
+    console.log(p);
 
     var userZip = p.profiles[0].supporter.address[0].zip;
     var userCity = p.profiles[0].supporter.address[0].city;
@@ -116,7 +177,6 @@ exports.authenticate = async (req, res) => {
     res.cookie("user_zip", p.profiles[0].supporter.address[0].zip);
     res.cookie("user_id", p.id);
     res.cookie("access_token", s.access_token).redirect(state);
-
   } catch (error) {
     res.status(400).json({
       success: false,
@@ -124,7 +184,6 @@ exports.authenticate = async (req, res) => {
     });
   }
 };
-
 
 const fetchToken = async code => {
   try {
@@ -141,7 +200,7 @@ const fetchProfile = async access_token => {
   try {
     const { data } = await Axios.get(
       "https://stage.vivaconagua.org/drops/oauth2/rest/profile?access_token=" +
-      access_token
+        access_token
     );
 
     return data;
@@ -161,7 +220,16 @@ const fetchEvents = async code => {
   }
 };
 
-
+const getDistance = async (poolEventGeo, userGeo) => {
+  try {
+    var data = geolib.getDistance(poolEventGeo, userGeo);
+    console.log("Distance: ", distanceInKm);
+    // resolve(distanceInKm);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
 
 exports.receiveToken = (req, res) => {
   res.json({});
